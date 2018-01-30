95441e6bc83f91d57a4ec18fa0051a1fabe9979a
e-henry <e-henry@users.noreply.github.com>
2018-01-30 14:00:29 +0100

    Init

---
+++ bin/run.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+'use strict'
+const chalk = require('chalk')
+
+// We want to have console output in the CLI app
+/* eslint-disable no-console */
+
+console.log(chalk.green('Hello World'))
---
+++ test/run.js
@@ -0,0 +1,31 @@
+'use strict'
+const node = process.execPath
+const cp = require('child_process')
+const execFile = cp.execFile
+const bin = require.resolve('../bin/run.js')
+const tap = require('tap')
+
+
+const run = (args, options, cb) => {
+  if (options && options.env) {
+    options.env = Object.keys(process.env).reduce((env, k) => {
+      if (env[k] === undefined) {
+        env[k] = process.env[k]
+      }
+      return env
+    }, options.env)
+  }
+
+  return execFile(node, [bin].concat(args), options, cb)
+}
+
+tap.test('Simple use', (t) => {
+  t.test('with no args', (t) => {
+    run([], {env:{_TAP_IS_TTY: '1'}}, (er, o, e) => {
+      t.match(er, null)
+      t.match(o, /^Hello World/)
+      t.end()
+    })
+  })
+  t.end()
+})
